721d540e3ca0185593f9495abfdb354bcb62a355
halfminami <129936969+halfminami@users.noreply.github.com>
2023-11-24 22:27:29 +0900

    update

--- src/fancy/index.tsx
+++ src/fancy/index.tsx
@@ -1,41 +1,49 @@
-import { render } from "preact";
+import { render } from 'preact';
 
-export const timelineTitle = "Profile thing";
-export const softwareLandingTitle = "Landing Page thing";
+export const timelineTitle = 'Profile thing';
+export const softwareLandingTitle = 'Landing Page thing';
 function Index() {
   return (
-    <ul>
-      <li>
-        <a href="/fancy/software-landing.html">{softwareLandingTitle}</a>
-        <p>cool dark theme something. animation</p>
-        <p>
-          icons are from{" "}
-          <a href={"https://fonts.google.com/icons"}>
-            Google Fonts Material Icons
-          </a>
-        </p>
-        <p>
-          logo font is from <a href="https://fonts.google.com/">Google Fonts</a>
-        </p>
-      </li>
+    <>
+      <ul>
+        <li>
+          <a href="/fancy/software-landing.html">{softwareLandingTitle}</a>
+          <p>cool dark theme something. animation</p>
+          <p>
+            icons are from{' '}
+            <a href={'https://fonts.google.com/icons'}>
+              Google Fonts Material Icons
+            </a>
+          </p>
+          <p>
+            logo font is from{' '}
+            <a href="https://fonts.google.com/">Google Fonts</a>
+          </p>
+        </li>
 
-      <li>
-        <a href="/fancy/timeline.html">{timelineTitle}</a>
-        <p>something like social media profile. vertically long images</p>
-        <p>
-          icons are from{" "}
-          <a href={"https://fonts.google.com/icons"}>
-            Google Fonts Material Icons
-          </a>
-        </p>
-        <p>
-          logo font is from <a href="https://fonts.google.com/">Google Fonts</a>
-        </p>
-        <p>
-          pictures are from <a href="https://picsum.photos/">Lorem Picsum</a>
-        </p>
-      </li>
-    </ul>
+        <li>
+          <a href="/fancy/timeline.html">{timelineTitle}</a>
+          <p>something like social media profile. vertically long images</p>
+          <p>
+            icons are from{' '}
+            <a href={'https://fonts.google.com/icons'}>
+              Google Fonts Material Icons
+            </a>
+          </p>
+          <p>
+            logo font is from{' '}
+            <a href="https://fonts.google.com/">Google Fonts</a>
+          </p>
+          <p>
+            pictures are from <a href="https://picsum.photos/">Lorem Picsum</a>
+          </p>
+        </li>
+      </ul>
+      <p>
+        I will be more likely doing such things on{' '}
+        <a href="https://codepen.io/halfminami">my CodePen</a> now, though.
+      </p>
+    </>
   );
 }
 
